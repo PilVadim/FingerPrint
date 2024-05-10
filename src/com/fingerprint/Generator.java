6667461588d05dee4611c529f2e08c681418588f
pilvadim <pilvadim@gmail.com>
2024-05-10 19:40:41 +0300

    extend description

--- src/com/fingerprint/Generator.java
+++ src/com/fingerprint/Generator.java
@@ -26,41 +26,37 @@ public class Generator {
         for (int i = 0; i < matrix.length; i++){
             for (int j = 0; j < matrix.length; j++){
 
-                final Boolean top;  //check if top element has bottom connection
-                if (i - 1 < 0){
+                final Boolean top; //check if top element has bottom connection
+                if (i - 1 < 0)
                     top = false;
-                } else if (matrix[i - 1][j] == null) {
+                else if (matrix[i - 1][j] == null)
                     top = null;
-                } else {
+                else
                     top = (matrix[i - 1][j].getCode() & 0b0010) > 0;
-                }
 
                 final Boolean right; //check if right element has left connection
-                if (j + 1 == matrix.length) {
+                if (j + 1 == matrix.length)
                     right = false;
-                } else if (matrix[i][j + 1] == null) {
+                else if (matrix[i][j + 1] == null)
                     right = null;
-                } else {
+                else
                     right = (matrix[i][j + 1].getCode() & 0b0001) > 0;
-                }
 
                 final Boolean bottom; //check if bot element has top connection
-                if (i + 1 == matrix.length) {
+                if (i + 1 == matrix.length)
                     bottom = false;
-                } else if (matrix[i + 1][j] == null) {
+                else if (matrix[i + 1][j] == null)
                     bottom = null;
-                } else {
+                else
                     bottom = (matrix[i + 1][j].getCode() & 0b1000) > 0;
-                }
 
                 final Boolean left; //check if right element has left connection
-                if (j - 1 < 0) {
+                if (j - 1 < 0)
                     left = false;
-                } else if (matrix[i][j - 1] == null) {
+                else if (matrix[i][j - 1] == null)
                     left = null;
-                } else {
+                else
                     left = (matrix[i][j - 1].getCode() & 0b0100) > 0;
-                }
 
                 matrix[i][j] = style.getRandomBrickByFilter(top, right, bottom, left);
             }
